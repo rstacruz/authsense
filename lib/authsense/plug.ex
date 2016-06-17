5c28bcf928f28584c9581b91f46166193be673be
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-06-17 15:55:22 +0800

    Use pattern matching for Authsense.Plug

--- lib/authsense/plug.ex
+++ lib/authsense/plug.ex
@@ -8,17 +8,17 @@ defmodule Authsense.Plug do
   @doc """
   Adds `:current_user` to the assigns.
   """
-  def assign_current_user(%{model: model, repo: repo}, conn, _options \\ nil) do
-    if Map.has_key?(conn.assigns, :current_user) do
-      conn
-    else
-      case get_session(conn, :current_user_id) do
-        nil ->
-          assign(conn, :current_user, nil)
-        id ->
-          user = repo.get!(model, id)
-          assign(conn, :current_user, user)
-      end
+  def assign_current_user(_, %Plug.Conn{assigns: %{ current_user: _ }} = conn, _) do
+    conn
+  end
+
+  def assign_current_user(%{model: model, repo: repo}, conn, _options) do
+    case get_session(conn, :current_user_id) do
+      nil ->
+        assign(conn, :current_user, nil)
+      id ->
+        user = repo.get!(model, id)
+        assign(conn, :current_user, user)
     end
   end
 end
