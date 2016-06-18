8b9a60ee25657981d9dbdbca4251a0436c63beb6
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-06-18 09:33:21 +0800

    Make set_current_user assign as well

--- lib/authsense/actions.ex
+++ lib/authsense/actions.ex
@@ -1,16 +1,20 @@
 defmodule Authsense.Actions do
   import Plug.Conn, only:
-    [put_session: 3, delete_session: 2]
+    [put_session: 3, delete_session: 2, assign: 3]
 
   @doc """
   See `Authsense.set_current_user/2`.
   """
   def set_current_user(conn, nil) do
-    delete_session(conn, :current_user_id)
+    conn
+    |> delete_session(:current_user_id)
+    |> assign(:current_user, nil)
   end
 
   def set_current_user(conn, user) do
     id = Map.get(user, :id)
-    put_session(conn, :current_user_id, id)
+    conn
+    |> put_session(:current_user_id, id)
+    |> assign(:current_user, user)
   end
 end
