3778580e95880273698dd9b00b6451245319fa7a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-06-24 16:51:19 +0800

    Update docs

--- lib/authsense/plug.ex
+++ lib/authsense/plug.ex
@@ -52,20 +52,21 @@ defmodule Authsense.Plug do
   @doc """
   Sets the current user for the session.
 
+      import Authsense.Plug
+
       conn
-      |> Auth.put_current_user(user)
+      |> put_current_user(user)
       |> put_flash(:info, "Welcome.")
       |> redirect(to: "/")
 
   To logout, set it to nil.
 
       conn
-      |> Auth.put_current_user(nil)
+      |> put_current_user(nil)
       |> put_flash(:info, "You've been logged out.")
       |> redirect(to: "/")
 
-  This sets the `:current_user_id` in the Session store. To access the User
-  model, use `Auth` as a plug (see `Authsense.Plug`).
+  This sets the `:current_user_id` in the Session store.
   """
   def put_current_user(conn, nil) do
     conn
