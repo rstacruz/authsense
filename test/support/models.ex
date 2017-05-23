d8cabd1923f3f0b1b21a9692c99ec6f33977310b
jekku <jekripreclaroorlina@gmail.com>
2017-05-23 12:13:59 +0800

    Extends mocks to multiple pieces of data.

--- test/support/models.ex
+++ test/support/models.ex
@@ -3,6 +3,7 @@ defmodule Authsense.Test.User do
   schema "" do
     field :email, :string
     field :hashed_password, :string
+    field :extra_field, :string
     timestamps
   end
 end
--- test/support/repo.ex
+++ test/support/repo.ex
@@ -6,16 +6,16 @@ defmodule Authsense.Test.Repo do
   end
   def get(_model, _id), do: nil
 
-  def get_by(_model, email: "nobody@gmail.com"), do: nil
-  def get_by(_model, email: "rico@gmail.com") do
-    valid_resource "rico@gmail.com", "foobar"
-  end
+  def get_by(model, email: email), do: Enum.find(model || mock_data(), &(&1.email == email))
 
-  def where(_model, extra_field: "unicorn") do
-    valid_resource "rico@gmail.com", "foobar"
-  end
+  def where(_model, extra_field: extra_field), do: Enum.filter(mock_data(), &(&1.extra_field == extra_field))
 
-  def where(_model, extra_field: extra_field), do: nil
+  defp mock_data do
+    [
+      %User{id: 1, email: "rico@gmail.com", hashed_password: crypto.hashpwsalt("foobar"), extra_field: "unicorn"},
+      %User{id: 2, email: "jekku@gmail.com", hashed_password: crypto.hashpwsalt("foobar"), extra_field: "newbie"}
+    ]
+  end
 
   defp valid_resource(email, password) do
     %User{id: 1, email: email, hashed_password: crypto.hashpwsalt(password)}
