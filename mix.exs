6d5dbe2667e0e29c8be3289eeb787e327db28961
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2017-09-27 19:57:00 +0800

    Add pbkdf2_elixir

--- mix.exs
+++ mix.exs
@@ -31,20 +31,12 @@ defmodule Authsense.Mixfile do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:ecto, "~> 2.2"},
       {:plug, "~> 1.4"},
       {:comeonin, "~> 4.0"},
+      {:pbkdf2_elixir, "~> 0.12", only: :dev},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev}
     ]
